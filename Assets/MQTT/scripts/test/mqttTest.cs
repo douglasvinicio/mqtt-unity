f745b922044e823cea2344ade07f99b77363e88e
Douglas Vinicio <douglas.vinicio@prevu3d.com>
2020-08-26 14:01:29 -0800

    update

--- Assets/MQTT/scripts/test/mqttTest.cs
+++ Assets/MQTT/scripts/test/mqttTest.cs
@@ -1,138 +1,97 @@
 ﻿using System;
-using System.Net;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
-
-//Used parts of the M2MQTT library
+using Newtonsoft.Json;
 using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
-
-//Not used yet - To discover parts of the library
-using uPLibrary.Networking.M2Mqtt.Utility;
-using uPLibrary.Networking.M2Mqtt.Exceptions;
-
-
-
-
+using System.Collections.Generic;
+using System.Linq;
 
 public class mqttTest : MonoBehaviour {
 
-    //Text Fields
-    public Text messageReceived; //Text to show message received from the broker
-	public Text mqttClientID; //Client ID 
+	//Broker hard coded info
+	string messageMQTT, brokerAdress = "prevu3d.intelligenceindustrielle.com", topic = "from_pump1", username="prevu3d", password="WJb3zbfzmM3*5^"; 
+	int port = 1883;
+	public string itemKey, itemValue;
+	//Controllers : Time and Loops
+	public float timeRemaining = 10;
+	public bool run;
+	//Input, Text, Game Objects 
+	public Text messageReceived, mqttClientID, TimeRemaining;
+	public Button btnClick;
+	public InputField brokerAdressInput, brokerPortInput, topicInput;
+	private MqttClient client;
+	public GameObject verticalLayout, templateVerticalElement;
+	public List<Text> newFieldCreated;
 
-    // Variables 
-	string messageMQTT; //Stores message received from the broker
-	string brokerAdress = " "; //Adress of the broker
-	string topic = "/"; //Name of the subscribed topic 
-	int port = 1883; //Port of the broker
-	
-    //Input Fields
-	public InputField brokerAdressInput; //User inputs the broker adress
-	public InputField portBrokerInput; //User input for the broker port
-	public InputField topicInput; //User input for the topic subscribed
-
-	//Controller
-	Boolean run = false;
-
-    //Button
-	public Button btnClick; //Button to send user input data
-
-    // New MQTT client 
-	private MqttClient client; //New MQTT client
-
-
-	void Start () {
-
-		//Button to create a new client
-		btnClick.onClick.AddListener(GetInputOnClick);
-
-        // Create client instance - Broker Adress , Port , Secure? , Certificate 
-			client = new MqttClient("localhost", 1883, false, null);
-
-			// Register to message received 
-			client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
-
-			//Client ID - This program is a client connected to the broker receiving data from another client
-			string clientId = Guid.NewGuid().ToString();
-			client.Connect(clientId);
-			mqttClientID.text = "Client ID: " + clientId;
-
-
-			// Subscribe to the TOPIC with QoS 2 
-			client.Subscribe(new string[] { "SomeTopic/" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
-
-
-
-    }
-
-
-	public void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e){
-
-		Debug.Log("Received: " + System.Text.Encoding.UTF8.GetString(e.Message));
-        messageMQTT = System.Text.Encoding.UTF8.GetString(e.Message);
-
-	}
-
-    // Button to get values from input field on click 
-	public void GetInputOnClick()
+	void Start()
 	{
-
-		run = true;
-		Debug.Log(run);
-        //Broker Adress
-		brokerAdress = brokerAdressInput.text;
-
-        //Topic to be subscribed assigned to variable topic
-		topic = topicInput.text;
-
-        
-        //Port input conversion to int and assigned to variable port
-        port = Convert.ToInt32(portBrokerInput.text);
-        
-
-
-		Debug.Log("Broker Adress : " + brokerAdress);
-        Debug.Log("Port: " + port);
-		Debug.Log("Topic: " + topic);
-
+		btnClick.onClick.AddListener(GetInputOnClick);
 	}
 
 
-	// Update
 	void Update()
+	{
+		//Timer update
+		if (timeRemaining > 0)
+		{
+			timeRemaining -= Time.deltaTime;
+		}
+		else
+		{
+            RawMessage();
+
+			ParseJson();
+
+			timeRemaining = 5;
+		}
+
+		CreateNewClient();
+		TimeRemaining.text = timeRemaining.ToString();
+	}
+
+	public void ParseJson()
     {
-        //Checking for messages received by the broker 
-		messageReceived.text = "Received from " + topic + " " + messageMQTT;
+		//Json to Dictionary using Json Newton 
+		var parsedMessages = JsonConvert.DeserializeObject<Dictionary<string, object>>(messageMQTT);
 
-        //Checking state of run
-		Debug.Log(run);
 
-        /* Creating a new client using user data input - run is set as false untill the button is clicked it then returns
-         * all the variables assigned with user input and initiates a new client - The difference from the first one is the assigned variables */
+		//Creating new UI fields for each pair key/value inside the Dictionary 
+		foreach (KeyValuePair<string, object> item in parsedMessages)
+		{
+			//Counting List<newFieldCreated> and creating new fields according to kvp inside the dictionary
+			int fieldsCounter = newFieldCreated.Count;
+			if (fieldsCounter < parsedMessages.Count)
+			{
+				//Instatiating new Text elements (cloning)
+				GameObject fields = Instantiate(templateVerticalElement, verticalLayout.transform);
+				fields.GetComponent<Text>().text = item.Key + " : " + item.Value;
+				newFieldCreated.Add(fields.GetComponent<Text>());
+			}
+		};
+
+		//Updating each new UI created with the new info.
+		for (int i = 0; i < newFieldCreated.Count; i++)
+		{
+			Text field = newFieldCreated[i];
+			itemKey = parsedMessages.ElementAt(i).Key;
+			itemValue = parsedMessages.ElementAt(i).Value.ToString();
+			field.text = i + " - " + itemKey + " : " + itemValue;
+		}
+	}
+	//Create new subscriber / Also new client ID
+	public void CreateNewClient()
+	{
 		switch (run)
 		{
 			case true:
-                //Testing purposes
-				Debug.Log("New client created");
-
-				// Create client instance // args = (Broker Adress , Port , Secure? , Certificate?) // ? = Still not know what it means inside this context 
-				client = new MqttClient(IPAddress.Parse(brokerAdress), port, false, null);
-
-				// Register to message received 
+				client = new MqttClient(brokerAdress, port, false, null);
 				client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
-
-				//Client ID - This creates a new client ID
 				string clientId = Guid.NewGuid().ToString();
-				client.Connect(clientId);
+				client.Connect(clientId, username, password);
 				mqttClientID.text = "Client ID: " + clientId;
-
-
-				// Subscribe to the TOPIC input by the user
 				client.Subscribe(new string[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
 
-                //Assiging run to false to break
 				run = false;
 				break;
 
@@ -140,7 +99,28 @@ public class mqttTest : MonoBehaviour {
 				break;
 		}
 	}
+
+	public void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+	{
+		messageMQTT = System.Text.Encoding.UTF8.GetString(e.Message);
+
+	}
+
+
+	//Subscribe Button - creates new client connection / getting input data and assigning to variables. 
+	public void GetInputOnClick()
+	{
+		run = true;
+		//brokerAdress = brokerAdressInput.text;
+		//topic = topicInput.text;
+		//port = Convert.ToInt32(portBrokerInput.text);
+
+	}
+
+	//Display not parsed message
+	public void RawMessage()
+	{
+		messageReceived.text = "Message received from " + topic + ": " + messageMQTT;
+	}
 }
-
-
 
